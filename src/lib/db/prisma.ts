e1cb4dc2b12ce5c78c962900dfe3194ea52375f4
Marcio Gabriel Holm Monteiro <Marci.Holm@MacBook-Air-de-Marcio-Holm.local>
2026-04-18 18:12:41 -0300

    Fix: Add fallback DATABASE_URL in prisma client

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -2,10 +2,17 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres:hI494IxoEtu4a00Z@db.vlpbichjuuttxhfepjil.supabase.co:5432/postgres';
+
 export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
         log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+        datasources: {
+            db: {
+                url: databaseUrl,
+            },
+        },
     });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
